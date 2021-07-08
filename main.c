c97585ab16eda54aa64bca13edfddf4384b9eca1
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-07-07 23:01:45 -0500

    Excentricidad y orientacion parabola

--- main.c
+++ main.c
@@ -19,35 +19,41 @@ int main() {
   printf("\n La ecuacion de segundo grado es :");
   printf("\n %dx^2 + %dxy + %dy^2 + %dx + %dy + %d = 0",a,b,c,d,e,f);
 
-  k = pow (b,2) - 4 * c * c;
+  k = pow (b,2) - 4 * a * c;
   printf("\n El valor del discriminante es: \n k=%d",k);
+
+  //Calculamos el radio
+  aux1 = pow(d,2) + pow(e,2) - (4+f);
+  radio = ( sqrt(aux1)) /2;
+  //Calculamos su excentricidad
+  aux2 = 1 - (a/c);
+  excen = sqrt(aux2);
 
   if (k <= 0){
 
     //printf("\n La seccion conica pertenece a una elipse o un circulo");
     //Punto 1.
-    //Primer evaluacion 
+    //Primer evaluacion A=C=1 y B= 0
     if(a == 1 && c == 1 && b == 0){
-      //Calculamos el radio
-      aux1 = pow(d,2) + pow(e,2) - (4+f);
-      radio = ( sqrt(aux1)) /2;
+      
       printf("\n La cónica es una circunferencia ");
       printf("\n Su radio es:  %.2f",radio);
-      //Calculamos su excentricidad
-      aux2 = 1 - (a/c);
-      excen = sqrt(aux2);
       printf("\n Su excentricidad es:  %.2f",excen);
 
     } else {
       printf("La cónica pertenece a una elipse");
     }
-  } else if(k == 0){
+  } else if(k == 0 || (a == 0 && b != 0) || (a != 0 && b==0)){
     printf("\n La seccion cónica pertenece a una parabola");
     // Punto 2
-    //Segunda evaluacion
-    if ( (a == 0 && b != 0) || (a != 0 && b==0)){
-
-    }
+    //Segunda evaluacion A=0 ó B= 0 (no ambas)
+    //excentricidad
+    printf("\n Su excentricidad es:  %.2f",excen);
+    //Orientacion de la parabola
+    if (a > 0 )
+      printf("\n La parabola esta orientada hacia arriba ");
+    else
+      printf("\n La parabola esta orientada hacia abajo");
   } else if(k >= 0){
     printf("\n La seccion cónica pertenece a una hiperbola");
   } else{
